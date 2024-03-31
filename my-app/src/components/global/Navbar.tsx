a6140278287541221670f7badb7443801b51a9d8
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-01 00:12:33 +0330

    ...

--- my-app/src/components/global/Navbar.tsx
+++ my-app/src/components/global/Navbar.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function Navbar() {
   return (
-    <header>
-      <aside></aside>
+    <header className="fixed top-0 right-0 left-0 py-4 px-4 bg-black/40 border-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+      <aside className="flex items-center gap-[2px]">
+        <p className="text-3xl font-bold">Fu</p>
+        <img
+          src=""
+          width={15}
+          height={15}
+          alt="fuzzy logo"
+          className="shadow-sm"
+        />
+        <p className="text-3xl font-bold">zie</p>
+      </aside>
     </header>
   );
 }
