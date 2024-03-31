9d9de9dbc2ecc0fdc98caea3da0c1eda359110f0
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-01 00:07:08 +0330

    ...

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/global/Navbar";
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
---
+++ my-app/src/components/global/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header>
+      <aside></aside>
+    </header>
+  );
+}
+
+export default Navbar;
