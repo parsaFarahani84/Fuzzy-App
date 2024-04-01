b4992653c4228f1abe228963a2171ef35e32a345
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-01 16:21:19 +0330

    ...

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,28 @@
 import Navbar from "@/components/global/Navbar";
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col">
+                <Button
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                  size={"lg"}
+                >
+                  Start Free Today
+                </Button>
+              </div>
+            }
+          ></ContainerScroll>
+        </div>
+      </section>
     </main>
   );
 }
--- my-app/src/components/global/Navbar.tsx
+++ my-app/src/components/global/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import { MenuIcon } from "lucide-react";
 
 function Navbar() {
   return (
@@ -6,13 +8,53 @@ function Navbar() {
       <aside className="flex items-center gap-[2px]">
         <p className="text-3xl font-bold">Fu</p>
         <img
-          src=""
+          src="/fuzzieLogo.png"
           width={15}
           height={15}
           alt="fuzzy logo"
           className="shadow-sm"
         />
         <p className="text-3xl font-bold">zie</p>
+      </aside>
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Products</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Documentations</Link>
+          </li>
+          <li>
+            <Link href="#">Enterprise</Link>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
+      </nav>
+      <aside>
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 "
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />{" "}
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {
+              //WIP
+              true ? "Dashboard" : "Get Started"
+            }
+          </span>
+        </Link>
+        <MenuIcon className="md:hidden" />
       </aside>
     </header>
   );
