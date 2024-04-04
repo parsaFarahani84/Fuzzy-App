87521413a1456ba0b5f1d85a0bc77308c9b2df69
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-04 12:22:53 +0330

    ...

---
+++ my-app/src/app/(main)/(pages)/connections/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Connections({}: Props) {
+  return <div>Connections</div>;
+}
+
+export default Connections;
---
+++ my-app/src/app/(main)/(pages)/workflows/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function WorkFlow({}: Props) {
+  return <div>WorkFlow</div>;
+}
+
+export default WorkFlow;
