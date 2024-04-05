e8b569080089d33a607062289eb224d53a1f6f36
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-05 15:54:32 +0330

    ...

--- my-app/src/app/(main)/(pages)/settings/page.tsx
+++ my-app/src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,25 @@
+import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
 
 type Props = {};
 
 function Settings({}: Props) {
-  return <div>Settings</div>;
+  // WIP: profile picture
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-cnter justify-between border-b bg-background/50 p-6 text-4xl border-blur-lg">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <h1 className="text-2xl font-bold">User Profile</h1>
+        <p className="text-abse text-white/50 mt-[-2.5rem]">
+          {" "}
+          Add or update your information
+        </p>
+      </div>
+      <ProfileForm />
+    </div>
+  );
 }
 
 export default Settings;
---
+++ my-app/src/components/forms/profile-form.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+function ProfileForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  return <div>ProfileForm</div>;
+}
+
+export default ProfileForm;
